ee3b72ebb4ca355488bf06affda6b67ec913b7d7
Jon Cram <webignition@gmail.com>
2019-01-10 10:18:53 +0000

    Remove WebPageInspector::CHARSET_* constants (#18)

--- src/WebPageInspector.php
+++ src/WebPageInspector.php
@@ -7,10 +7,6 @@ use webignition\WebResourceInterfaces\WebPageInterface;
 
 class WebPageInspector
 {
-    const CHARSET_GB2312 = 'GB2312';
-    const CHARSET_BIG5 = 'BIG5';
-    const CHARSET_UTF_8 = 'UTF-8';
-
     /**
      * @var WebPageInterface
      */
