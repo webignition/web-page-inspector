3dc886f5454a0e398086cf7992209b760346b54f
Jon Cram <webignition@gmail.com>
2019-02-14 12:26:45 +0000

    Add IeConditionalCommentInspector::extractData() (#33)

--- src/IeConditionalCommentInspector.php
+++ src/IeConditionalCommentInspector.php
@@ -4,10 +4,29 @@ namespace webignition\WebPageInspector;
 
 class IeConditionalCommentInspector
 {
-    const IE_CONDITIONAL_COMMENT_IDENTIFICATION_PATTERN = '/^\[if[^>]+]>(?:(?!<!\[endif\]).)+<!\[endif\]$/ms';
+    const START_PATTERN = '\[if[^>]+\]>';
+    const END_PATTERN = '<!\[endif\]';
+    const IDENTIFICATION_PATTERN =
+        '/^' . self::START_PATTERN . '(?:(?!' . self::END_PATTERN . ').)+' . self::END_PATTERN . '$/ms';
 
     public function isIeConditionalComment(\DOMComment $commentNode): bool
     {
-        return preg_match(self::IE_CONDITIONAL_COMMENT_IDENTIFICATION_PATTERN, $commentNode->data) > 0;
+        return preg_match(self::IDENTIFICATION_PATTERN, $commentNode->data) > 0;
+    }
+
+    public function extractData(\DOMComment $commentNode): string
+    {
+        if (!$this->isIeConditionalComment($commentNode)) {
+            return '';
+        }
+
+        return trim(preg_replace(
+            [
+                '/^' . self::START_PATTERN . '/',
+                '/' . self::END_PATTERN . '$/',
+            ],
+            '',
+            $commentNode->data
+        ));
     }
 }
