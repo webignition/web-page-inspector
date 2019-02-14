13cf647c996a3404fca88bc503748444203acd3d
Jon Cram <jon@webignition.net>
2019-02-14 12:23:38 +0000

    Tidy up identification patterns

--- src/IeConditionalCommentInspector.php
+++ src/IeConditionalCommentInspector.php
@@ -4,13 +4,14 @@ namespace webignition\WebPageInspector;
 
 class IeConditionalCommentInspector
 {
-    const START_PATTERN = '/^\[if[^>]+]>/';
-    const END_PATTERN = '/<!\[endif\]$/';
-    const IE_CONDITIONAL_COMMENT_IDENTIFICATION_PATTERN = '/^\[if[^>]+]>(?:(?!<!\[endif\]).)+<!\[endif\]$/ms';
+    const START_PATTERN = '\[if[^>]+\]>';
+    const END_PATTERN = '<!\[endif\]';
+    const IDENTIFICATION_PATTERN =
+        '/^' . self::START_PATTERN . '(?:(?!' . self::END_PATTERN . ').)+' . self::END_PATTERN . '$/ms';
 
     public function isIeConditionalComment(\DOMComment $commentNode): bool
     {
-        return preg_match(self::IE_CONDITIONAL_COMMENT_IDENTIFICATION_PATTERN, $commentNode->data) > 0;
+        return preg_match(self::IDENTIFICATION_PATTERN, $commentNode->data) > 0;
     }
 
     public function extractData(\DOMComment $commentNode): string
@@ -21,8 +22,8 @@ class IeConditionalCommentInspector
 
         return trim(preg_replace(
             [
-                self::START_PATTERN,
-                self::END_PATTERN,
+                '/^' . self::START_PATTERN . '/',
+                '/' . self::END_PATTERN . '$/',
             ],
             '',
             $commentNode->data
